51cb234c62b6280f129054022272c9996e8e93a7
Jesande <58031707+Jesande@users.noreply.github.com>
2020-07-05 20:59:35 +0200

    add remove btn position

--- solutions/RemoveBtn.js
+++ solutions/RemoveBtn.js
@@ -1,7 +1,5 @@
 
 // select the element (the remove btn)
 
-$(".wcs-shopping-item-remove").remove();
-$(".wcs-shopping-item-qty").after("<span class='spd-wcs-shopping-item-remove'> Remove </span>");
+$(".wcs-shopping-item-remove").css({"top":"70%","right":"-130px;"});
 
-$(".wcs-shopping-item-qty").after("<span data-element-id='wcs-remove-item58372542' value='' class='wcs-shopping-item-remove' rel-order='28574403' rel='58372542' rel-configurationid='88634770' rel-roxlensandserviceset='' rel-roxset='' rel-roxexpressnotavail='false' catentryid='51001' itemno=''> Remove </span>");
