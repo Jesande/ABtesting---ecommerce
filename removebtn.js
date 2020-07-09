46b0b770cb346f69b4227a3c31cc25835aa2da98
Jesande <58031707+Jesande@users.noreply.github.com>
2020-07-09 08:55:46 +0200

    try to change the css - fail

---
+++ removebtn.js
@@ -0,0 +1,4 @@
+
+// select the element 
+$("[data-element-id='wcs-remove-item58372542']").removeClass(".wcs-shopping-item-remove");
+$("[data-element-id='wcs-remove-item58372542']").css({"position":"absolute", "bottom":"0", "right":"337px", "font-size": "13px", "color":"#666", "text-decoration": "underline", "cursor": "pointer", "width": "66px", "border": "1px solid #d0c7c7", "padding": "0 5px"});
