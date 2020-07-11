dfd33841c8e8838892de0d6d18111ae3a9d75c10
Jesande <58031707+Jesande@users.noreply.github.com>
2020-07-11 20:34:45 +0200

    position removebtn

--- removebtn.js
+++ removebtn.js
@@ -1,4 +1,6 @@
+// positioning the element
+$(".wcs-shopping-item-remove").css("top","70%");
+$(".wcs-shopping-item-remove").css("right","-130px");
 
-// select the element 
-$("[data-element-id='wcs-remove-item58372542']").removeClass(".wcs-shopping-item-remove");
-$("[data-element-id='wcs-remove-item58372542']").css({"position":"absolute", "bottom":"0", "right":"337px", "font-size": "13px", "color":"#666", "text-decoration": "underline", "cursor": "pointer", "width": "66px", "border": "1px solid #d0c7c7", "padding": "0 5px"});
+//delete de X
+...
